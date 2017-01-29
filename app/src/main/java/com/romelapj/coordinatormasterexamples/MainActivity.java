6db4f7c7da48992ffa89bc9e51461f7db03b7bed
romelapj <romelpalomino@hotmail.com>
2017-01-28 19:49:41 -0500

    Simple Coordinator Layout

--- app/src/main/java/com/romelapj/coordinatormasterexamples/MainActivity.java
+++ app/src/main/java/com/romelapj/coordinatormasterexamples/MainActivity.java
@@ -1,7 +1,9 @@
 package com.romelapj.coordinatormasterexamples;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
@@ -11,9 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //App bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.appbar_main);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Coordinator Master Layout");
     }
 
     public void showCoordinatorLayout(View view) {
-        Toast.makeText(this, "Mostrar CoordinatorLayout", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(MainActivity.this, SimpleCoordinatorLayoutActivity.class));
     }
 }
